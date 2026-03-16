64e4a0a72afdfcb3793bcd4efdc9d2215ac272c5
timo-zzz <timothyluu08@gmail.com>
2026-03-16 17:11:50 -0500

    vision stuffs

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -7,7 +7,7 @@
 
 package frc.robot.subsystems.vision;
 
-import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -17,7 +17,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 public class VisionConstants {
   // AprilTag layout
   public static AprilTagFieldLayout aprilTagLayout =
-      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+      AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
 
   // Camera names, must match names configured on coprocessor
   public static String frontCameraName = "front";
@@ -32,34 +32,37 @@ public class VisionConstants {
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d frontCameraTransform =
       new Transform3d(
-          Inches.of(((10.592370 + 12.090440) / 2)),
-          Inches.of(0.4),
-          Inches.of(15.651053),
+          Meters.of(0.748 - 0.413),
+          Meters.of(1.897 - 1.7526),
+          Meters.of(0.261),
           new Rotation3d(0.0, -18.0 * Math.PI / 180, 0.0));
   public static Transform3d backCameraTransform =
       new Transform3d(
-          Inches.of(-((11.3 + 12.77) / 2)),
-          Inches.of(-((6.21 + 4.9) / 2)),
-          Inches.of(16.052 + 3.57),
-          new Rotation3d(0.0, -18.0 * Math.PI / 180, 0.0)
-              .rotateBy(new Rotation3d(0.0, 0.0, Math.PI)));
+          //   Inches.of(-((11.3 + 12.77) / 2)),
+          //   Inches.of(-((6.21 + 4.9) / 2)),
+          //   Inches.of(16.052 + 3.57),
+          //   new Rotation3d(0.0, -18.0 * Math.PI / 180, 0.0)
+          //       .rotateBy(new Rotation3d(0.0, 0.0, Math.PI))
+          );
   public static Transform3d rightCameraTransform =
       new Transform3d(
-          Inches.of(-3.5),
-          Inches.of(14.5),
-          Inches.of(21),
-          new Rotation3d(0.0, -30.0 * Math.PI / 180, 0.0)
-              .rotateBy(new Rotation3d(0.0, 0.0, -Math.PI / 2.0)));
+          //   Inches.of(-3.5),
+          //   Inches.of(14.5),
+          //   Inches.of(21),
+          //   new Rotation3d(0.0, -30.0 * Math.PI / 180, 0.0)
+          //       .rotateBy(new Rotation3d(0.0, 0.0, -Math.PI / 2.0))
+          );
   public static Transform3d leftCameraTransform =
       new Transform3d(
-          Inches.of((1.881 + 3.269) / 2),
-          Inches.of((13.662 + 15.152) / 2),
-          Inches.of(10.332 + 3.57),
-          new Rotation3d(0.0, -30.0 * Math.PI / 180, 0.0)
-              .rotateBy(new Rotation3d(0.0, 0.0, Math.PI / 2.0)));
+          //   Inches.of((1.881 + 3.269) / 2),
+          //   Inches.of((13.662 + 15.152) / 2),
+          //   Inches.of(10.332 + 3.57),
+          //   new Rotation3d(0.0, -30.0 * Math.PI / 180, 0.0)
+          //       .rotateBy(new Rotation3d(0.0, 0.0, Math.PI / 2.0))
+          );
 
   // Basic filtering threshold
-  public static double maxAmbiguity = 0.3;
+  public static double maxAmbiguity = 0.1;
   public static double maxZError = 0.75;
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
