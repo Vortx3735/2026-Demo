b296734d20cb937c3d91f80d278a4ae4137747ee
rkoripalli <rkoripalli@hotmail.com>
2026-04-07 01:02:27 -0500

    Format WLEDController code for improved readability and consistency

--- src/main/java/frc/robot/util/WLEDController.java
+++ src/main/java/frc/robot/util/WLEDController.java
@@ -6,27 +6,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class WLEDController {
-    private final HttpClient client = HttpClient.newHttpClient();
-    private final String url;
+  private final HttpClient client = HttpClient.newHttpClient();
+  private final String url;
 
-    public WLEDController(String ip) {
-        this.url = "http://" + ip + "/json/state";
-    }
+  public WLEDController(String ip) {
+    this.url = "http://" + ip + "/json/state";
+  }
 
-    public void setPreset(int id) {
-        send("{\"ps\":" + id + "}");
-    }
+  public void setPreset(int id) {
+    send("{\"ps\":" + id + "}");
+  }
 
-    public void setPower(boolean on) {
-        send("{\"on\":" + on + "}");
-    }
+  public void setPower(boolean on) {
+    send("{\"on\":" + on + "}");
+  }
 
-    private void send(String json) {
-        HttpRequest request = HttpRequest.newBuilder()
+  private void send(String json) {
+    HttpRequest request =
+        HttpRequest.newBuilder()
             .uri(URI.create(url))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString(json))
             .build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.discarding());
-    }
+    client.sendAsync(request, HttpResponse.BodyHandlers.discarding());
+  }
 }
